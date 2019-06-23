6dccd688eeecfb37f1c4eb485fcbd87f4d206404
Timothee Groleau <timothee.groleau@gmail.com>
2019-06-23 17:06:52 +0800

    TEMP: Adding ezcap calibration file

---
+++ calibration_osx_ezcap_videograbber.py
@@ -0,0 +1,10 @@
+
+WINDOW_NAME = "ezcap VideoGrabber"
+CAPTURE_COORDS = (28,22,585,470)
+scorePerc = (0.754,0.264,0.187,0.032)
+linesPerc = (0.598,0.094,0.092,0.032)
+levelPerc = (0.818,0.705,0.060,0.032)
+statsPerc = (0.188,0.399,0.091,0.434)
+curDasPerc = (0.16,0.16,0.060,0.032)
+curPiecePerc = (0.068,0.212,0.088,0.05)
+curPieceDasPerc = (0.223,0.23,0.060,0.032)
