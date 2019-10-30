28f3d0dea119f3d3c1ffec39773c7ab9adbc5785
Timothee Groleau <tgroleau@zendesk.com>
2019-10-30 20:40:44 +0800

    Do actual conditional import in NetworkClient

--- Networking/NetworkClient.py
+++ Networking/NetworkClient.py
@@ -1,10 +1,8 @@
 from config import config
-import Networking.TCPClient as TCPClient
-import Networking.AutoBahnClient as AutoBahnClient
-import Networking.FileClient as FileClient
 
-NetClient = TCPClient
 if config.netProtocol == 'AUTOBAHN' or config.netProtocol == 'AUTOBAHN_V2':
-	NetClient = AutoBahnClient
+	import Networking.AutoBahnClient as NetClient
 elif config.netProtocol == 'FILE':
-    NetClient = FileClient
+	import Networking.FileClient as NetClient
+else:
+	import Networking.TCPClient as NetClient
